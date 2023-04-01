3ca805ded369053b3c31413a23da9cb814531f9d
Bruno Meneses <brnmeneses@gmail.com>
2023-04-01 12:20:37 -0300

    changes in the Cards component

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -10,24 +10,37 @@ function Feature({ title, desc, ...rest }: any) {
 }
 
 export default function Cards() {
+  const features = {
+    profile: {
+      title: "Profile",
+      desc: "34 yrs old, married from Londrina-BR",
+    },
+    education: {
+      title: "Education",
+      desc: "Graduated since 2010. Fullstack engenieer. English enthusiast.",
+    },
+    professional: {
+      title: "Professional",
+      desc: "Expertise in many areas like Education, Oil&Energy, Financial",
+    },
+    goals: {
+      title: "Goals",
+      desc: "I would like to visit every country in the world",
+    },
+  };
+
   return (
-    <HStack spacing={8}>
+    <HStack spacing={2} p={2}>
+      <Feature title={features.profile.title} desc={features.profile.desc} />
       <Feature
-        title="Plan Money"
-        desc="The future can be even brighter but a goal without a plan is just a wish"
+        title={features.education.title}
+        desc={features.education.desc}
       />
       <Feature
-        title="Save Money"
-        desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process"
+        title={features.professional.title}
+        desc={features.professional.desc}
       />
-      <Feature
-        title="Save Money"
-        desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process"
-      />
-      <Feature
-        title="Save Money"
-        desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process"
-      />
+      <Feature title={features.goals.title} desc={features.goals.desc} />
     </HStack>
   );
 }
